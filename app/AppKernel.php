7ecf0f6302469fa141e9f0a41fe3de6652a15dad
PEM-FR <pe.manteau@gmail.com>
2015-07-21 15:09:26 +0200

    removed FosUser. we will add it again later

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
             new Pem\BookBundle\PemBookBundle(),
         );
 
